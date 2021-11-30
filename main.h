854ee588e7e9e2eee357220473331bb99854436d
M Rifqi M <akuanakke1@gmail.com>
2021-12-01 00:10:33 +0700

    Add files via upload

---
+++ main.h
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+int main();
+void menu_prosedur();
+void menu_identitas(int *i,int del);
+void input_pendaftar(int i);
+void output_pendaftar(int i);
+void menu_ujian_teori(int i);
+void daftar_hasil_ujian_teori(int i);
+void menu_ujian_praktik(int i);
+void menu_input_ujian_praktik(int i);
+void daftar_hasil_ujian_praktik(int i);
+void input_nilai_sim_a(int i);
+void input_nilai_sim_b1(int i);
+void input_nilai_sim_b2(int i);
+void input_nilai_sim_c(int i);
+void input_nilai_sim_d(int i);
+void input_nilai_sim_d_a(int i);
+void input_nilai_sim_d_c(int i);
+void menu_formulir(int i);
+void administrasi(int i);
+void reorganisasi(int *i,int *del);
+void edit_data(int i);
+void cek_kebenaran_data(int i);
+void cetak_sim(int i, int *k);
+
---
+++ nilai_uji_praktik.h
@@ -0,0 +1,53 @@
+void nilai_sim_A()
+{
+	cout<<"Nilai Praktik SIM A"<<endl;
+	cout<<"-------------------"<<endl;
+	cout<<"1. Persiapan mengemudi (drill cockpit)"<<endl;
+	cout<<"2. Menjalankan kendaraan bermotor uji maju \n   dan mundur sejauh 50 meter pada jalur sempit"<<endl;
+	cout<<"3. Uji slalom"<<endl;
+	cout<<"4. Parkir paralel dan parkir seri"<<endl;
+	cout<<"5. Tanjakan dan turunan"<<endl<<endl;
+}
+
+void nilai_sim_B1()
+{
+	cout<<"Nilai Praktik SIM B1"<<endl;
+	cout<<"-------------------"<<endl;
+	cout<<"1. Persiapan mengemudi (drill cockpit)"<<endl;
+	cout<<"2. Menjalankan kendaraan bermotor uji maju \n   dan mundur sejauh 50 meter pada jalur sempit"<<endl;
+	cout<<"3. Uji slalom"<<endl;
+	cout<<"4. Parkir paralel dan parkir seri"<<endl;
+	cout<<"5. Tanjakan dan turunan"<<endl<<endl;
+}
+
+void nilai_sim_B2()
+{
+	cout<<"Nilai Praktik SIM B2"<<endl;
+	cout<<"-------------------"<<endl;
+	cout<<"1. Persiapan mengemudi (drill cockpit)"<<endl;
+	cout<<"2. Menjalankan kendaraan bermotor uji maju \n   dan mundur sejauh 50 meter pada jalur sempit"<<endl;
+	cout<<"3. Uji slalom"<<endl;
+	cout<<"4. Parkir paralel dan parkir seri"<<endl;
+	cout<<"5. Tanjakan dan turunan"<<endl<<endl;
+}
+
+void nilai_sim_C()
+{
+	cout<<"Nilai Praktik SIM C"<<endl;
+	cout<<"-------------------"<<endl;
+	cout<<"1. Uji keseimbangan"<<endl;
+	cout<<"2. Uji slalom"<<endl;
+	cout<<"3. Uji angka 8"<<endl;
+	cout<<"4. Uji reaksi"<<endl;
+	cout<<"5. Uji berbalik arah membentuk huruf U"<<endl<<endl;
+}
+
+void nilai_sim_D()
+{
+	cout<<"Nilai Praktik SIM D"<<endl;
+	cout<<"-------------------"<<endl;
+	cout<<"1. Persiapan mengemudi (drill cockpit)"<<endl;
+	cout<<"2. Menjalankan kendaraan bermotor uji maju \n   dan mundur sejauh 50 meter pada jalur sempit"<<endl;
+	cout<<"3. Parkir paralel dan parkir seri"<<endl;
+	cout<<"4. Tanjakan dan turunan"<<endl<<endl;
+}
